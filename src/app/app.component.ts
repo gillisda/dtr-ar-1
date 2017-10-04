9e9321041cb477a2ff2eb30e1c987b243ce4b07d
Don Gillis <gillisda@gmail.com>
2017-10-04 16:42:19 -0400

    Completed POC for Firebase Cloud Firestore.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import firebase from 'firebase';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -17,6 +19,23 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+ 
+    firebase.initializeApp({
+      apiKey: "AIzaSyBV44BvvSO5qRU2Ytw1PtIJB2Yml2z9X4c",
+      authDomain: "augmented-voice-167112.firebaseapp.com",
+      databaseURL: "https://augmented-voice-167112.firebaseio.com",
+      projectId: "augmented-voice-167112",
+      storageBucket: "augmented-voice-167112.appspot.com",
+      messagingSenderId: "855884425303"
+    });
+    
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+ 
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +11,18 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    
+        var db = firebase.firestore();
+    
+        db.collection("dtr2").add({
+          username: "dg@dg.com" 
+          
+        })
+          .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+          })
+          .catch(function (error) {
+            console.error("Error adding document: ", error);
+          });
   }
-
 }
